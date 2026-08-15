9b682c51f16f9777aa2389f0160ade5a1739b9f2
Sajal-26 <schitlangia26@gmail.com>
2026-08-15 15:10:22 +0530

    Added login

--- authentication/users/serializers.py
+++ authentication/users/serializers.py
@@ -1,5 +1,7 @@
 from rest_framework import serializers
+from rest_framework_simplejwt.tokens import RefreshToken
 from .models import User
+from django.contrib.auth import authenticate
 
 class RegisterSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only = True, min_length = 8)
@@ -24,3 +26,27 @@ class RegisterSerializer(serializers.ModelSerializer):
         user.save()
 
         return user
+
+
+class LoginSerializer(serializers.Serializer):
+    email = serializers.EmailField()
+    password = serializers.CharField(write_only = True)
+
+    def validate(self, attrs):
+        email = attrs['email']
+        password = attrs['password']
+
+        user = authenticate(
+            username = email,
+            password = password
+        )
+
+        if user is None:
+            raise serializers.ValidationError('Invalid username or password')
+        
+        refresh = RefreshToken.for_user(user)
+
+        return {
+            'access' : str(refresh.access_token),
+            'refresh' : str(refresh)
+        }
